956a9dcdec5ca9c0dd9b5d1bd1cdb657c28355ed
wcfx <wcfx.dev@gmail.com>
2021-03-25 21:55:01 -0300

    chore: styled-components, eslint, babel-imports, configuracoes padroes de projeto

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <Style.Container>
+      <Style.Title>Hello Canadá</Style.Title>
+    </Style.Container>
+  )
+}
+
+export default Home;
