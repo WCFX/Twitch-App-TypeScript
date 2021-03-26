f5ffb507e84659800a45a557fa14463aafa276eb
wcfx <wcfx.dev@gmail.com>
2021-03-26 01:01:20 -0300

    refactor: refatorando o eslint. estrutura concluida; iniciando a config das features

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import 'react-native-gesture-handler';
-
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
+
+import Routes from './routes';
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Routes />
+      <StatusBar style="light" backgroundColor="#221c35" />
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import * as View from '~/views';
@@ -7,7 +9,7 @@ const { Navigator, Screen } = createStackNavigator();
 
 const AuthRoutes = () => {
   return (
-    <Navigator initialRouteName="Home">
+    <Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={View.Home} />
     </Navigator>
   );
