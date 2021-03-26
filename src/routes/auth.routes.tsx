8eb4a134feca7df238c2c537bcd3b136cbda28f0
wcfx <wcfx.dev@gmail.com>
2021-03-26 02:00:44 -0300

    style: tabbar modificada com as propriedades de estilizacao

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,27 +1,92 @@
 import React from 'react';
 
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import colors from '~/styles/colors';
 import * as View from '~/views';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const AuthRoutes = () => {
   return (
-    <Navigator>
+    <Navigator
+      tabBarOptions={{
+        style: {
+          height: 60,
+          backgroundColor: colors.primary,
+          borderTopWidth: 0,
+        },
+        tabStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Roboto400',
+          fontSize: 11,
+          marginTop: 5,
+        },
+        inactiveTintColor: colors.black,
+        activeTintColor: colors.purple,
+      }}
+    >
       <Screen
         options={{
           tabBarIcon: ({ size, focused }) => (
-            <Ionicons name="md-heart" size={26} />
+            <Ionicons
+              name="md-heart"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
           ),
         }}
         name="Following"
         component={View.Following}
       />
-      <Screen name="Browse" component={View.Browse} />
-      <Screen name="Discover" component={View.Discover} />
-      <Screen name="Esports" component={View.Esports} />
+      <Screen
+        options={{
+          tabBarIcon: ({ size, focused }) => (
+            <Ionicons
+              name="md-browsers"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+          ),
+        }}
+        name="Browse"
+        component={View.Browse}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ size, focused }) => (
+            <MaterialCommunityIcons
+              name="compass-outline"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+          ),
+        }}
+        name="Discover"
+        component={View.Discover}
+      />
+      <Screen
+        options={{
+          tabBarIcon: ({ size, focused }) => (
+            <MaterialCommunityIcons
+              name="trophy-outline"
+              size={size}
+              color={focused ? colors.purple : colors.black}
+            />
+          ),
+        }}
+        name="Esports"
+        component={View.Esports}
+      />
     </Navigator>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,18 +26,18 @@ import AuthRoutes from './auth.routes';
 
 const Routes = () => {
   const [fontsLoaded] = useFonts({
-    Nunito_400Regular,
-    Nunito_600SemiBold,
-    Nunito_700Bold,
-    Poppins_400Regular,
-    Poppins_600SemiBold,
-    Poppins_700Bold,
-    Archivo_400Regular,
-    Archivo_600SemiBold,
-    Archivo_700Bold,
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
+    Nunito400: Nunito_400Regular,
+    Nunito600: Nunito_600SemiBold,
+    Nunito700: Nunito_700Bold,
+    Poppins400: Poppins_400Regular,
+    Poppins600: Poppins_600SemiBold,
+    Poppins700: Poppins_700Bold,
+    Archivo400: Archivo_400Regular,
+    Archivo600: Archivo_600SemiBold,
+    Archivo700: Archivo_700Bold,
+    Roboto400: Roboto_400Regular,
+    Roboto500: Roboto_500Medium,
+    Roboto700: Roboto_700Bold,
   });
 
   if (!fontsLoaded) {
